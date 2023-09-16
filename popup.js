37425dd48781e8db10042aa89960da770d68aef8
Sepay <pintorodrigo28@gmail.com>
2023-09-16 15:06:38 +0100

    running in the background and reset done

--- popup.js
+++ popup.js
@@ -1,19 +1,66 @@
+let originalMinute = 25;  // Original minute value
+let originalSecond = 0;   // Original second value
+let intervalId;  // Variable to store the interval ID
 
+let minute = originalMinute;  // Current minute value
+let second = originalSecond;  // Current second value
 
-var minute = 24
-var second = 60
+// Helper function to format time (add leading zero if needed)
+function formatTime(time) {
+  return time < 10 ? `0${time}` : `${time}`;
+}
 
-setInterval(function(){
-    if(minute == 0 && second ==1){
-        document.getElementById("counter").innerHTML = "00:00"
-    }else{
+document.getElementById('startButton').addEventListener('click', () => {
+  // Reset the timer to the original time (25 minutes)
+  minute = originalMinute;
+  second = originalSecond;
+
+  // Clear the existing interval if any
+  clearInterval(intervalId);
+
+  // Format the time and update the counter display in the popup
+  document.getElementById('counter').innerText = `${formatTime(minute)}:${formatTime(second)}`;
+
+  // Send a message to the background script to start the timer
+  chrome.runtime.sendMessage({ type: 'startTimer' });
+
+  // Start a new interval with the original timer values
+  intervalId = setInterval(() => {
+    if (minute === 0 && second === 1) {
+      // Stop the timer when it reaches 00:00
+      clearInterval(intervalId);
+    } else {
+      if (second === 0) {
+        minute--;
+        second = 59;
+      } else {
         second--;
-        if(second == 0){
-            minute--;
-            second=60;
-        }if(minute==0){
-            minute=minute;
-        }
+      }
     }
-    document.getElementById("counter").innerHTML = minute + ":" + second
-},1000)
+    // Format the time and update the counter display in the popup
+    document.getElementById('counter').innerText = `${formatTime(minute)}:${formatTime(second)}`;
+  }, 1000);
+});
+
+document.getElementById('stopButton').addEventListener('click', () => {
+  // Send a message to the background script to stop the timer
+  chrome.runtime.sendMessage({ type: 'stopTimer' });
+
+  // Reset the timer to the original time (25 minutes)
+  minute = originalMinute;
+  second = originalSecond;
+
+  // Clear the existing interval
+  clearInterval(intervalId);
+
+  // Format the time and update the counter display in the popup
+  document.getElementById('counter').innerText = `${formatTime(minute)}:${formatTime(second)}`;
+});
+
+// Listen for messages from the background script
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.type === 'updateCounter') {
+    // Format the time and update the counter display in the popup
+    document.getElementById('counter').innerText = message.counter;
+  }
+});
