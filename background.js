06a8977c29fc017184db649075f8de3b06848802
Sepay <pintorodrigo28@gmail.com>
2023-09-18 12:27:50 +0100

    circle timer done

--- background.js
+++ background.js
@@ -1,52 +1,12 @@
-let intervalId;
-let isBreakTime = false;
+// Code for managing background behavior
 
-function startTimer(minutes, seconds) {
-  intervalId = setInterval(() => {
-    if (minutes === 0 && seconds === 0) {
-      if (isBreakTime) {
-        // Send a message to the popup to update the counter for the break
-        chrome.runtime.sendMessage({ type: 'updateCounter', counter: '00:00' });
-
-        // Reset to the original timer for the next work session
-        minutes = 24;
-        seconds = 60;
-        isBreakTime = false;
-      } else {
-        // Send a message to the popup to indicate the end of the work session
-        chrome.runtime.sendMessage({ type: 'workSessionEnded' });
-
-        // Switch to break time (5 minutes)
-        minutes = 4;
-        seconds = 60;
-        isBreakTime = true;
-
-        // Notify popup to start the break timer
-        chrome.runtime.sendMessage({ type: 'startBreakTimer' });
-      }
-    } else {
-      seconds--;
-      if (seconds === 0) {
-        minutes--;
-        seconds = 60;
-      }
-    }
-
-    // Send a message to the popup to update the counter
-    chrome.runtime.sendMessage({ type: 'updateCounter', counter: `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}` });
-  }, 1000);
-}
-
-function stopTimer() {
-  clearInterval(intervalId);
-}
-
-// Listen for messages from the popup
+// Listen for messages from the popup or other parts of the extension
 chrome.runtime.onMessage.addListener((message) => {
   if (message.type === 'startTimer') {
-    stopTimer(); // Reset the timer before starting
-    startTimer(isBreakTime ? 4 : 24, 60);
+    // Handle starting the timer (if needed)
+    // You can add your logic here for handling the start of the timer
   } else if (message.type === 'stopTimer') {
-    stopTimer();
+    // Handle stopping the timer (if needed)
+    // You can add your logic here for handling the stop of the timer
   }
 });
--- popup.js
+++ popup.js
@@ -5,6 +5,9 @@ let intervalId;  // Variable to store the interval ID
 let minute = originalMinute;  // Current minute value
 let second = originalSecond;  // Current second value
 
+const circle = document.getElementById('hh');  // Get the circle element
+const counter = document.getElementById('counter');  // Get the counter element
+
 // Helper function to format time (add leading zero if needed)
 function formatTime(time) {
   return time < 10 ? `0${time}` : `${time}`;
@@ -21,8 +24,8 @@ document.getElementById('startButton').addEventListener('click', () => {
   // Format the time and update the counter display in the popup
   document.getElementById('counter').innerText = `${formatTime(minute)}:${formatTime(second)}`;
 
-  // Send a message to the background script to start the timer
-  chrome.runtime.sendMessage({ type: 'startTimer' });
+  // Reset the circle's dasharray to create a full border
+  circle.style.strokeDasharray = `${circle.getTotalLength()}`;
 
   // Start a new interval with the original timer values
   intervalId = setInterval(() => {
@@ -37,15 +40,23 @@ document.getElementById('startButton').addEventListener('click', () => {
         second--;
       }
     }
+
+    // Calculate the remaining time in seconds
+    const totalTimeInSeconds = originalMinute * 60;
+    const remainingTimeInSeconds = minute * 60 + second;
+
+    // Calculate the dasharray value to make the border disappear
+    const dasharrayValue = (remainingTimeInSeconds / totalTimeInSeconds) * circle.getTotalLength();
+
+    // Update the circle's stroke-dasharray property
+    circle.style.strokeDasharray = `${dasharrayValue} ${circle.getTotalLength()}`;
+
     // Format the time and update the counter display in the popup
     document.getElementById('counter').innerText = `${formatTime(minute)}:${formatTime(second)}`;
   }, 1000);
 });
 
 document.getElementById('stopButton').addEventListener('click', () => {
-  // Send a message to the background script to stop the timer
-  chrome.runtime.sendMessage({ type: 'stopTimer' });
-
   // Reset the timer to the original time (25 minutes)
   minute = originalMinute;
   second = originalSecond;
@@ -53,14 +64,9 @@ document.getElementById('stopButton').addEventListener('click', () => {
   // Clear the existing interval
   clearInterval(intervalId);
 
+  // Reset the circle's dasharray to create a full border
+  circle.style.strokeDasharray = `${circle.getTotalLength()}`;
+
   // Format the time and update the counter display in the popup
   document.getElementById('counter').innerText = `${formatTime(minute)}:${formatTime(second)}`;
-});
-
-// Listen for messages from the background script
-chrome.runtime.onMessage.addListener((message) => {
-  if (message.type === 'updateCounter') {
-    // Format the time and update the counter display in the popup
-    document.getElementById('counter').innerText = message.counter;
-  }
 });
